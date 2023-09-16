7230fb9eec40b6a7775353a1cd5a574feefe8536
Mark Armanious <mark@getmysa.com>
2023-09-16 21:11:30 -0230

    standardize color with COLORS constant

---
+++ src/Constants/Colors.ts
@@ -0,0 +1,6 @@
+const COLORS = {
+  background: "#242424",
+  text: "rgba(255, 255, 255, 0.87)",
+  highlight: "#646cff",
+};
+export default COLORS;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import COLORS from "./Constants/Colors.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+const root: HTMLElement = document.documentElement;
+root.style.backgroundColor = COLORS.background;
+root.style.color = COLORS.text;
