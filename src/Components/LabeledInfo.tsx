d276109960d95a89a201c8e8634c1741451eab9a
Mark Armanious <mark@getmysa.com>
2023-09-18 22:44:14 -0230

    Improve LabeledInfo CSS

--- src/Components/LabeledInfo.tsx
+++ src/Components/LabeledInfo.tsx
@@ -9,6 +9,7 @@ interface LabeledInfoProps {
 
 const LabeledInfoContainer = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: row;
 
@@ -16,8 +17,9 @@ const LabeledInfoContainer = styled.div`
 `;
 const InfoContainer = styled.div`
   width: 100%;
-  height: 100%;
   flex: 1;
+  padding: 5px;
+  flex-grow: 2;
   background-color: ${COLORS.secondary};
   border-radius: 4px;
 `;
@@ -26,6 +28,7 @@ const LabelContainer = styled.div`
   height: 100%;
   border-radius: 8px;
   flex: 3;
+  flex-shrink: 2;
   text-align: left;
   padding: 5px;
   padding-left: 5%;
